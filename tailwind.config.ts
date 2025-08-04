4b26707bcd1d600a93585454d91f2b638bab94dd
Mohamed AIT SIDI HOU <maitsidihou@actinvision.com>
2025-08-04 15:13:19 +0200

    cleaning

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-
+import tailwindAnimate from "tailwindcss-animate";
 export default {
 	darkMode: ["class"],
 	content: [
@@ -140,5 +140,5 @@ export default {
 			}
 		}
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [tailwindAnimate],
 } satisfies Config;
